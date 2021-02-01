3636693e19923c791d284c41f7edf354e3ef578d
DDimitr0va <desi.dim@yahoo.com>
2021-02-01 17:13:07 +0200

    Code

--- src/main/java/com/pet/MainPet.java
+++ src/main/java/com/pet/MainPet.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MainPet {
     public static void main(String[] args) {
-        System.out.println("What is your pet's name?");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your pet's name?");
         String name = scanner.nextLine();
         System.out.println("What do your pet like to do?");
         String action = scanner.nextLine();
-        System.out.println("My pet's name is " + name + " and it likes " + action);
+        System.out.println("My pet's name is " + name + " and it likes " + action + ".");
     }
 }
